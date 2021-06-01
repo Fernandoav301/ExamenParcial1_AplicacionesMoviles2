2aee32f59e2d153b1119c73ccd3947b9ee7c6906
Fernandoav301 <Fernandoemanuelavila03@gmail.com>
2021-05-31 23:31:11 -0500

    pantallas creadas

---
+++ app/src/main/java/com/example/cuatrimestre6_parcial1/pantalla_saturno.java
@@ -0,0 +1,14 @@
+package com.example.cuatrimestre6_parcial1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantalla_saturno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_saturno);
+    }
+}
